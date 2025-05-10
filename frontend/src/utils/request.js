060414685c14ac92177c1f53f4650072cf03eee3
mahmoud2ali <98770789+mahmoud2ali@users.noreply.github.com>
2025-05-10 12:58:40 +0300

    Update request.js

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,9 +2,9 @@ import axios from "axios"
 
 const request = axios.create({
     //Deployment URL
-    // baseURL: "https://event-booking-system-rhka.onrender.com/api", 
+     baseURL: "https://event-booking-system-rhka.onrender.com/api", 
     // Localhost URL
-    baseURL: "http://localhost:8000/api",
+    // baseURL: "http://localhost:8000/api",
 });
 
 export default request;
